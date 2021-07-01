6848653ccd39013bde329c446df73b13a1f876b8
Marzouq <>
2021-07-01 18:08:30 +0000

    new

---
+++ app.js
@@ -0,0 +1,20 @@
+// var x=2
+// var y=9
+// var sum=x+y
+// console.log(sum)
+// 
+// var n1 = "haneen "
+// var n2="                   daad"
+// var name = n1+n2+     "                    1"
+// console.log(name)
+
+// if (20 < 15) {
+//   console.log('yes')
+// }
+// else if (15 > 20) {
+//   console.log('no')
+// }
+// else{
+//   console.log('hehe')
+// }
+alert ('hi')
